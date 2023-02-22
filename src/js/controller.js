e4bf7369d5db6967487efe0ae571e3e637a71f6f
ugochukwuSylva <sylvaspecial@gmail.com>
2023-02-22 15:46:17 +0100

    God is the Greatest

--- src/js/controller.js
+++ src/js/controller.js
@@ -114,10 +114,6 @@ const controlSubmitrecipe = async function (newRecipe) {
   }
 };
 
-const welcomeMsg = function () {
-  console.log('Welcome ✨!');
-};
-
 const init = function () {
   bookmarkView.addHandlerStoreBookmark(bookmarkStorage);
   recipeView.renderListnerEvent(controllerRecipe);
@@ -126,6 +122,5 @@ const init = function () {
   searchView.renderListnerEvent(controllerSearchResult);
   paginationView.renderListnerEvent(loadPagePreview);
   addRecipeView.addHandlerSubmitRecipe(controlSubmitrecipe);
-  welcomeMsg();
 };
 init();
